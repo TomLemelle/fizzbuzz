94084bdeef07031f095bee39c776dfd185169215
TomLemelle <tom.lemelle@hotmail.fr>
2021-09-21 12:14:15 +0200

    Ajoutez des fichiers projet.

---
+++ fizzbuzz/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                string FizzBuzzStr = "";
+                if (i % 3 == 0) FizzBuzzStr += "Fizz "; 
+                if (i % 5 == 0) FizzBuzzStr += "Buzz";
+                Console.WriteLine($"{i} {FizzBuzzStr}");
+            }
+        }
+    }
+}
